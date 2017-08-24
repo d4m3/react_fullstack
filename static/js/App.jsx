22070c0e6519a032117692e789f50ecefcda5391
DForbes <damianoforbes@gmail.com>
2017-08-24 19:01:55 -0400

    Initial Commit

---
+++ static/js/App.jsx
@@ -0,0 +1,30 @@
+// React APP
+import React from "react";
+import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
+
+require('../css/fullstack.css');
+var $ = require('jquery');
+
+import HeaderBackgroundImage from '../images/header.jpg';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    addHeaderImg() {
+        let headerBg = new Image();
+        headerBg.src = HeaderBackgroundImage;
+    }
+
+    render () {
+        return (
+            <PageHeader>
+                <div className='header-contents'>
+                {this.addHeaderImg()}
+                <Hello name='Damian' />
+                </div>
+            </PageHeader>
+        );
+    }
+}
